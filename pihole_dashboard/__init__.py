b81675918fb6ee196cbef02e5ec4d3c03bdf8e52
Uladzimir Kazakevich <kwull@kwull.com>
2023-01-12 17:05:31 +0100

    Two pi-holes mode introduced

--- pihole_dashboard/__init__.py
+++ pihole_dashboard/__init__.py
@@ -32,12 +32,15 @@ if os.geteuid() != 0:
     sys.exit("You need root permissions to access E-Ink display, try running with sudo!")
 
 INTERFACE = "wlan0"
-PIHOLE_PORT = 80
+PIHOLE_1_PORT = 85
+PIHOLE_1_ADDR = "192.168.1.250"
+
+PIHOLE_2_PORT = 80
+PIHOLE_2_ADDR = "192.168.68.250"
 
 OUTPUT_STRING = ""
 FILENAME = "/tmp/.pihole-dashboard-output"
 
-hostname = socket.gethostname()
 font_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'font')
 font_name = os.path.join(font_dir, "font.ttf")
 font16 = ImageFont.truetype(font_name, 16)
@@ -55,7 +58,7 @@ def valid_ip(address):
         return False
 
 
-def draw_dashboard(out_string=None):
+def draw_dashboard(status_string, out_string=None):
 
     image = Image.new("1", (epd.height, epd.width), 255)
     draw = ImageDraw.Draw(image)
@@ -64,23 +67,20 @@ def draw_dashboard(out_string=None):
     t = strftime("%H:%M:%S", localtime())
     time_string = "Updated: {}".format(t)
 
-    # Get Version
-    cmd = "/usr/local/bin/pihole -v"
-    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-    output = process.stdout.read().decode().split('\n')
-    version = output[0].split("(")[0].strip()
-
     draw.rectangle([(0, 105), (250, 122)], fill=0)
     if out_string is not None:
         draw.text((0, 0), out_string, font=font16, fill=0)
-    draw.text((5, 106), version, font=font12, fill=1)
+    draw.text((5, 106), status_string, font=font12, fill=1)
     draw.text((150, 106), time_string, font=font12, fill=1)
     epd.display(epd.getbuffer(image))
 
 
 def update():
-    url = "http://127.0.0.1:{}/admin/api.php".format(PIHOLE_PORT)
-    r = json.load(urllib.request.urlopen(url))
+    url = "http://{}:{}/admin/api.php".format(PIHOLE_1_ADDR, PIHOLE_1_PORT)
+    r1 = json.load(urllib.request.urlopen(url))
+    
+    url = "http://{}:{}/admin/api.php".format(PIHOLE_2_ADDR, PIHOLE_2_PORT)
+    r2 = json.load(urllib.request.urlopen(url))
 
     try:
         ip = ni.ifaddresses(INTERFACE)[ni.AF_INET][0]['addr']
@@ -88,27 +88,39 @@ def update():
         ip_str = "[×] Can't connect to Wi-Fi"
         ip = ""
 
-    if "unique_clients" not in r:
+    if "unique_clients" not in r1 or "unique_clients" not in r2:
         output_string = "Error from API.\nRun pihole-dashboard-draw\nfor details."
-        draw_dashboard(output_string)
-        output_error = "API Response: {}".format(r)
+        draw_dashboard('Error', output_string)
+        output_error = "API Response: {}\n {}".format(r1, r2)
         sys.exit(output_error)
+       
+    dns_queries_today_1 = r1['dns_queries_today']
+    dns_queries_today_2 = r2['dns_queries_today']
+    
+    ads_blocked_today_1 = r1['ads_blocked_today']
+    ads_blocked_today_2 = r2['ads_blocked_today']
+    
+    ads_percentage_today_1 = r1['ads_percentage_today']
+    ads_percentage_today_2 = r2['ads_percentage_today']
+    
+    domains_being_blocked_1 = r1['domains_being_blocked']
+    domains_being_blocked_2 = r2['domains_being_blocked']
+    
+    queries_cached_1 = r1['queries_cached']
+    queries_cached_2 = r2['queries_cached']
+    
+    status_1 = r1['status']
+    status_2 = r2['status']
+    
+    OUTPUT_STRING = "[✓] Total Queries: {}, (1) {} / (2) {}".format(dns_queries_today_1+dns_queries_today_2, dns_queries_today_1, dns_queries_today_2)
+    OUTPUT_STRING = OUTPUT_STRING + "\n" + "[✓] Queries Cached: {}, (1) {} / (2) {}".format( queries_cached_1+queries_cached_2, queries_cached_1, ads_percentage_today_2)
+    OUTPUT_STRING = OUTPUT_STRING + "\n" + "[×] Queries Blocked: {}, (1) {} / (2) {}".format(ads_blocked_today_1+ads_blocked_today_2, ads_blocked_today_1, ads_blocked_today_2)
+    OUTPUT_STRING = OUTPUT_STRING + "\n" + "[×] Percent Blocked: (1) {:.2%} / (2) {:.2%}".format(ads_percentage_today_1, ads_percentage_today_2)
+    OUTPUT_STRING = OUTPUT_STRING + "\n" + "[×] Blocklist: (1) {} / (2) {}".format(domains_being_blocked_1, domains_being_blocked_2)
 
-    unique_clients = r['unique_clients']
-    ads_blocked_today = r['ads_blocked_today']
-
-    if valid_ip(ip):
-        ip_str = "[✓] IP of {}: {}".format(hostname, ip)
-
-    cmd = "/usr/local/bin/pihole status"
-    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-    output = process.stdout.read().decode().split('\n')
-
-    OUTPUT_STRING = ip_str + "\n" + output[0].strip().replace('✗', '×') + "\n" + output[6].strip().replace('✗', '×')
-    OUTPUT_STRING = OUTPUT_STRING + "\n" + "[✓] There are {} clients connected".format(unique_clients)
-    OUTPUT_STRING = OUTPUT_STRING + "\n" + "[✓] Blocked {} ads".format(ads_blocked_today)
-
-    hash_string = hashlib.sha1(OUTPUT_STRING.encode('utf-8')).hexdigest()
+    STATUS_STRING = "(1) {} / (2) {}".format(status_1, status_2)
+    
+    hash_string = hashlib.sha1((OUTPUT_STRING+STATUS_STRING).encode('utf-8')).hexdigest()
     try:
         hash_file = open(FILENAME, "r+")
 
@@ -121,4 +133,4 @@ def update():
         hash_file.seek(0)
         hash_file.truncate()
         hash_file.write(hash_string)
-    draw_dashboard(OUTPUT_STRING)
+    draw_dashboard(STATUS_STRING, OUTPUT_STRING)
